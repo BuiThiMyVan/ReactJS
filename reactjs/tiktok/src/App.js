1ae616b66cc8c4cd6b61aaf557bfa887b2b8883b
vanbtm611 <myvanbui6112000@gmail.com>
2023-09-21 01:06:57 +0700

    two-way bindings

--- reactjs/tiktok/src/App.js
+++ reactjs/tiktok/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from'react'
+
+
+const courses = [
+  {
+    id: 1,
+    name: 'HTML, CSS'
+  },
+  {
+    id: 2,
+    name: 'JavaScript'
+  },
+  {
+    id: 3,
+    name: 'ReactJS'
+  }
+]
 
 function App() {
+  
+  const [checked, setChecked] = useState([])
+
+  console.log(checked)
+  const handleSubmit = () => {
+    console.log({id: checked})
+  }
+
+  const handleCheck = (id) => {
+    setChecked(prev => {
+      const isChecked = checked.includes(id)
+      if(isChecked) {
+        return checked.filter(item => item !== id)
+      } else {
+        return [...prev, id]
+      }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{padding: 32}}>
+      {
+        courses.map(course => (
+          <div key={course.id}>
+            <input type='checkbox' checked={checked.includes(course.id)} onChange={() => handleCheck(course.id)}/>
+            {course.name}
+          </div>
+        ))
+      }
+      <button onClick={handleSubmit}>Register</button>
     </div>
   );
 }
